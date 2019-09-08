243718bb176f06fe424a2ae70e1ed5c539d83318
MohammedAyman2018 <emidobibo20@gmail.com>
2019-09-08 04:56:56 +0200

    Improve add posts

--- js/main.js
+++ js/main.js
@@ -74,14 +74,13 @@ $(document).ready(function () {
         }
     }); 
 
-    $('#summernote').summernote({
-        popover: {
-            image: [],
-            link: [],
-            air: []
+
+    var editor = new MediumEditor('.editable',{
+        placeholder: {
+            /* This example includes the default options for placeholder,
+               if nothing is passed this is what it used */
+            text: 'أكتب شيئاً مفيداً',
         }
     });
-
-    
 
 });
