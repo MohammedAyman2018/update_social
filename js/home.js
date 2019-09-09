0ba90d796fb74fee222612d83ee1eb7304f34933
MohammedAyman2018 <emidobibo20@gmail.com>
2019-09-09 20:52:47 +0200

    Add (friends , posts) to profile_page layout

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    var editor = new MediumEditor('.editable',{
+            placeholder: {
+                /* This example includes the default options for placeholder,
+                   if nothing is passed this is what it used */
+                text: 'أكتب شيئاً مفيداً',
+            }
+        });
+    
+    });
--- js/main.js
+++ js/main.js
@@ -75,12 +75,9 @@ $(document).ready(function () {
     }); 
 
 
-    var editor = new MediumEditor('.editable',{
-        placeholder: {
-            /* This example includes the default options for placeholder,
-               if nothing is passed this is what it used */
-            text: 'أكتب شيئاً مفيداً',
-        }
+    // Images on modal
+    $('img').click(e => {
+        e.preventDefault();
+        
     });
-
 });
