781086f317635968dc6a8dc4ed345db549e93340
MohammedAyman2018 <emidobibo20@gmail.com>
2019-09-10 07:35:49 +0200

    Improve Add post && Fix responsive bugs && finish Profile layout

--- js/home.js
+++ js/home.js
@@ -1,11 +1,41 @@
 $(document).ready(function () {
 
     var editor = new MediumEditor('.editable',{
-            placeholder: {
-                /* This example includes the default options for placeholder,
-                   if nothing is passed this is what it used */
-                text: 'أكتب شيئاً مفيداً',
-            }
-        });
+        placeholder: {
+            /* This example includes the default options for placeholder,
+                if nothing is passed this is what it used */
+            text: 'أكتب شيئاً مفيداً',
+        }
+    });
+
+    var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+    if( screenWidth < 575 ){
+      $('.add-post').html('<i class="fas fa-plus"></i');
+    }
+
+    $('#summernote').summernote({
+        lang: 'ar-AR' ,
+        height: 300,
+        popover: {
+            image: [],
+            link: [],
+            air: []
+        },
+        hint: {
+            mentions: ["محمد", "أبوحميد", "تير_متير"],
+            match: /\B#(\w*)$/,
+            search: function (keyword, callback) {
+              callback($.grep(this.mentions, function (item) {
+                return item.indexOf(keyword) == 0;
+              }));
+            },
+            content: function (item) {
+              return '#' + item;
+            }    
+          }
+    });
+
+
     
     });
